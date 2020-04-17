a48d245b2f55b8cb3e6cafe07a6afbbb36c83f8a
SergioRibera <sergioalejandroriberacosta@gmail.com>
2020-04-17 19:01:00 -0400

    Init Commit

---
+++ js/app.js
@@ -0,0 +1,70 @@
+var dataProyect = {};
+window.onLoad = function(){
+    compile();
+}
+function compile() {
+	var html = document.getElementById("code-html");
+	var css = document.getElementById("code-css");
+	var js = document.getElementById("code-js");
+	var code = document.getElementById("show-code").contentWindow.document;
+
+    CodeMirror.hint.coffeescript = this;
+
+    document.body.onkeyup = function(){
+        code.open();
+        code.writeln(editorHtml.getValue()+"<style>"+editorCss.getValue()+"</style>"+"<script>" + editorJs.getValue() + "</script>");
+        code.close();
+    };
+};
+var editorJs = CodeMirror.fromTextArea(document.getElementById("code-js"), {
+    lineNumbers: true,
+    mode: "javascript",
+    keyMap: "sublime",
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    showCursorWhenSelecting: true,
+    theme: "monokai",
+    tabSize: 4,
+    extraKeys:{"Shift-Space":"autocomplete"}
+});
+editorJs.on('keyup', function(editor, event){
+    console.log()
+    if(event.key != "{" && event.key != "}" && event.key != "Backspace" &&
+        event.key != "Shift" && event.key != "ArrowLeft" && 
+        event.key != "ArrowRight" && event.key != "ArrowUp" && 
+        event.key != "ArrowDown" && event.key != "Enter" &&
+        event.key != ";" && event.key != "Control" && event.key != "CapsLock" &&
+        event.key != "1" && event.key != "2" && event.key != "3" &&
+        event.key != "4" && event.key != "5" && event.key != "6" &&
+        event.key != "7" && event.key != "8" && event.key != "7" && 
+        event.key != "0" && event.key != "{" && event.key != "}"){
+        CodeMirror.commands.autocomplete(editor);
+    }
+});
+var consoleEnginer = CodeMirror.fromTextArea(document.getElementById("console_enginer"), {
+    lineNumbers: true,
+    mode: "javascript",
+    showCursorWhenSelecting: true,
+    theme: "monokai",
+    readOnly: true,
+    disableInput: true
+});
+
+var input = document.getElementById("select-theme");
+
+function selectTheme() {
+	var theme = input.options[input.selectedIndex].textContent;
+	editorJs.setOption("theme", theme);
+	location.hash = "#" + theme;
+}
+var choice = (location.hash && location.hash.slice(1)) ||
+           (document.location.search &&
+            decodeURIComponent(document.location.search.slice(1)));
+if (choice) {
+	input.value = choice;
+	editorJs.setOption("theme", choice);
+}
+CodeMirror.on(window, "hashchange", function() {
+	var theme = location.hash.slice(1);
+	if (theme) { input.value = theme; selectTheme(); }
+});
